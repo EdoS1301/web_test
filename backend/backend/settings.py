a84627d5ef3ddc8a3586df65f154a5cc13450d3c
dadza <badzaguad@mail.ru>
2025-10-20 16:00:19 +0300

    new questions, colors on main page fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,7 +34,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'api/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'api/templates')],  # Добавьте эту строку
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
